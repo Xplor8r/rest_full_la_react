fd61104578ba1729dce67b809658d59c845d2cd5
Xplor8r <mpieschel39@gmail.com>
2019-01-08 13:23:52 -0800

    exceeded foursquare call quota

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,19 +13,20 @@ class RestaurantsController < ApplicationController
       end
       body = JSON.parse(response.body)
       restaurants = body["response"]["venues"]
-      restaurants.each do |restaurant|
-        id = restaurant["id"]
-        response = Faraday.get ('https://api.foursquare.com/v2/venues/' + id) do |req|
-          req.params['client_id'] = ENV['client_id']
-          req.params['client_secret'] = ENV['client_secret']
-          req.params['v'] = '20160201'
-        end
-          body = JSON.parse(response.body)
-          restaurantData = body["response"]["venue"]
-          restaurantList.push(restaurantData)
-          restaurantList
-      end
-     render json: restaurantList
+      # excedes foursquare premium calls quota
+      # restaurants.each do |restaurant|
+      #   id = restaurant["id"]
+      #   response = Faraday.get ('https://api.foursquare.com/v2/venues/' + id) do |req|
+      #     req.params['client_id'] = ENV['client_id']
+      #     req.params['client_secret'] = ENV['client_secret']
+      #     req.params['v'] = '20160201'
+      #   end
+      #     body = JSON.parse(response.body)
+      #     restaurantData = body["response"]["venue"]
+      #     restaurantList.push(restaurantData)
+      #     restaurantList
+      # end
+     render json: restaurants
 # binding.pry
   end
 end
