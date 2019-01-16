db4afc43ead8973f3ad2ee76bb6a09efcf6f02a3
Michael Pieschel <sixtyhectares@yahoo.com>
2019-01-15 19:31:52 -0800

    add media to hungry

--- client/src/components/hungry.js
+++ client/src/components/hungry.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Button, Col } from 'reactstrap';
+import { Button, Col, Media } from 'reactstrap';
 import  { Link } from 'react-router-dom'
-
+import restaurant from "../images/restaurant.jpg"
 class Hungry extends React.Component {
     render() {
         return (
             <Col className="text-center">
+                <Media top width="50%" src={restaurant} alt="Card image cap" />
                 <h5>Are you hungry? Need help finding a restaurant?</h5>
                 <Link to="/pick"><Button>Get a Random Pick</Button></Link>  <Link to="/restaurants"><Button>View a List of Restaunts</Button></Link>  <Link to="/goodbye"><Button>No Thanks</Button></Link>
             </Col>
