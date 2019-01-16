2bab6b5103246f83962e2884c320176e548054a1
Michael Pieschel <sixtyhectares@yahoo.com>
2019-01-15 19:29:37 -0800

    reactstrap goodbye with media

--- client/src/components/goodbye.js
+++ client/src/components/goodbye.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Button, Media, Col } from 'reactstrap';
 import  { Link } from 'react-router-dom'
+import restaurant from "../images/restaurant.jpg"
 
 class NoRestaurantFound extends React.Component {
     render() {
         return (
-            <div>
-                <h5>Goodbye</h5>
-                <p>Try searching for a restaurant at <a href="https://foursquare.com/" >Foursquare</a></p>
-
-                <Link to="/"><Button>Home</Button></Link>
-            </div>
+            <Col className="text-center">
+                <Media top width="50%" src={restaurant} alt="Card image cap" />
+                <Link to="/hungry"><Button>Please Come Back!</Button></Link>
+                <p>Or try searching for a restaurant at <a href="https://foursquare.com/" >Foursquare</a></p>
+            </Col>
+ 
         )
     }
 }
