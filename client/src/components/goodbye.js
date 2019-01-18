5158cb9071d41360547b8b490d876d00c62ccf0f
Xplor8r <mpieschel39@gmail.com>
2019-01-17 20:04:59 -0800

    more css remove restaurant image

--- client/src/components/goodbye.js
+++ client/src/components/goodbye.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Button, Media, Col } from 'reactstrap';
+import { Button, Col } from 'reactstrap';
 import  { Link } from 'react-router-dom'
-import restaurant from "../images/restaurant.jpg"
+
 
 const GoodBye = () => {
     return (
         <Col className="text-center">
-            <Media top width="50%" src={restaurant} alt="Card image cap" /><br/>
             <p>Try searching for a restaurant at <a href="https://foursquare.com/" >Foursquare</a></p>
             <Link to="/hungry"><Button>Please Come Back!</Button></Link>
         </Col>
--- client/src/components/hungry.js
+++ client/src/components/hungry.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Button, Col, Media } from 'reactstrap';
+import { Button, Col } from 'reactstrap';
 import  { Link } from 'react-router-dom'
-import restaurant from "../images/restaurant.jpg"
+
 
 const Hungry = () => {
     return (
         <Col className="text-center">
-            <Media top width="50%" src={restaurant} alt="Card image cap" />
+ 
             <h5>Are you hungry? Need help finding a restaurant?</h5>
             <Link to="/pick"><Button>Get a Random Pick</Button></Link>  <Link to="/restaurants"><Button>View a List of Restaunts</Button></Link>  <Link to="/goodbye"><Button>No Thanks</Button></Link>
         </Col>
