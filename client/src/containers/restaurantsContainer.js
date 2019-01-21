b62a1e74c3e20ad3e9b7dbe34d5fba356c6f8431
Michael Pieschel <mpieschel39@gmail.com>
2019-01-21 15:05:10 -0800

    create navbar for restaurants container

--- client/src/containers/restaurantsContainer.js
+++ client/src/containers/restaurantsContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Col, Container, Row } from 'reactstrap';
+import { Button, Container, Collapse, Navbar, NavbarToggler, Nav, NavItem } from 'reactstrap';
 import  { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Restaurants from '../components/restaurants';
@@ -24,18 +24,40 @@ const getRestaurants = (restaurants, filter) => {
 }
    
 export class RestaurantsContainer extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          isOpen: false
+        };
+      }
+      toggle() {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+      }
     render() {
         let restaurants = getRestaurants(this.props.restaurantData, this.props.filter)
         return (
             <Container>
-                <Row>
-                    <Col className="clearfix" style={{padding: '1rem'}}>
-                        <Filter />
-                        <Link to="/pick"><Button size="lg" color="danger">Get a Random Pick</Button></Link>{' '}
-                        <Link to="/hungry"><Button size="lg" color="danger">Back</Button></Link>{' '}
-                        <ModalForm />
-                    </Col>
-                </Row> 
+                <Navbar light expand="md" className="justify-content-sm-center">
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                    <Filter />
+                    <ModalForm />
+
+                    <NavItem>
+                        <Link to="/pick"><Button style={{ marginRight: '.5rem' }} size="lg" color="danger" >Get a Random Pick</Button></Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to="/hungry"><Button size="lg" color="danger">Back</Button></Link>
+                    </NavItem>
+                    </Nav>
+                    </Collapse>
+                    
+                    </Navbar>
                 {restaurants.length === 0? 
                     <NoDelivery />:
                     <Restaurants restaurants={restaurants} />}                        
